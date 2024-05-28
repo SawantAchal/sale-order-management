b658be734c387f9bb296a062728f1367e1f7272e
SawantAchal <achals1718@gmail.com>
2024-05-29 02:19:55 +0530

    implement login component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,21 +1,63 @@
-import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+    const [username , setUsername] = useState('')
+    const [password , setPassword] = useState('')
+    const [error , setError] = useState('')
+    const [usernameError , setUsernameError] = useState('')
+    const [passwordError , setPasswordError] = useState('')
+    const navigate = useNavigate()
+
+    const hangleLogin = () => {
+        if (!username) {
+            setUsernameError('username is required')
+        }
+        else{
+            setUsernameError('')
+        }
+
+        if (!password) {
+            setPasswordError('Password is required')
+        } else {
+            setPasswordError('')
+        }
+
+        if (username === 'user' && password === 'password') {
+            navigate('/')
+        } else {
+            setError('Invalid username and password')
+        }
+    }
+
+
   return (
     <>
-        <Box>
-            <form>
-                <FormControl>
+        <Box p={4} maxW={"md"} mx={'auto'} mt={"10%"}>
+            <VStack spacing={4}>
+                <Heading>Login</Heading>
+                {
+                    error && <Box color={'red.500'}>{error}</Box>
+                }
+                <form>
+                <FormControl isInvalid={usernameError}>
                     <FormLabel>UserName</FormLabel>
-                    <Input type='text' value=""/>
+                    <Input type='text' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    {
+                        usernameError && <FormErrorMessage>{usernameError}</FormErrorMessage>
+                    }
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={passwordError}>
                     <FormLabel>Password</FormLabel>
-                    <Input type='password' value=""/>
+                    <Input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    {
+                        passwordError && <FormErrorMessage>{passwordError}</FormErrorMessage>
+                    }
                 </FormControl>
-                <Button>Login</Button>
+                <Button colorScheme='blue' onClick={hangleLogin}>Login</Button>
             </form>
+            </VStack>
         </Box>
     </>
   )
